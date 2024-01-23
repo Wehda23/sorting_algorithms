712e169d37607e3fd8735441fef4baac92a1386d
Waheed Khaled <waheedkhaled95@gmail.com>
2024-01-24 00:30:37 +0200

    Test bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include "sort.h"
 
-
 /**
  * swap - function used to swap two values inside an array
- * first: First integer value
- * second: Second integer value
+ * @first: First integer value
+ * @second: Second integer value
  * Returns: Nothing.
  */
 void swap(int *first, int *second)
@@ -17,14 +16,32 @@ void swap(int *first, int *second)
 
 /**
  * bubble_sort - function that applies bubble sort algorithm.
- * array: Array to apply the algorithm on
- * size: size of the inserted array.
+ * @array: Array to apply the algorithm on
+ * @size: size of the inserted array.
  * Returns: Nothing.
  */
-void bubble_sort(int *array, size_t size){
-    /* Was estated that only sort array of size > 2*/
-    if (size > 2)
-    {
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, j;
+    int swapped;
 
+    if (size < 1)
+        return;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        swapped = 0;
+        for (j = 0; j < size - i - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                swap(&array[j], &array[j + 1]);
+                swapped = 1;
+                print_array(array, size);
+            }
+        }
+
+        if (!swapped)
+            break;
     }
 }
