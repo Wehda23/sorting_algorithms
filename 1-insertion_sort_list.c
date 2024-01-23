b46e023d49f753e73de37c3ce8a02cd503e4f0c1
Waheed Khaled <waheedkhaled95@gmail.com>
2024-01-24 00:49:10 +0200

    Preparing for insertion Sort Algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,2 +1,13 @@
 #include <stdio.h>
 #include "sort.h"
+
+
+
+/**
+ * insertion_sort_list - function that applies insertion sort algorithm.
+ * @list: Head of a doubly-linked list
+ * Returns: Nothing.
+ */
+void insertion_sort_list(listint_t **list){
+
+}
