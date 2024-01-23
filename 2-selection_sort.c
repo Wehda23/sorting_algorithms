461d072f3f3270dc853c84152c18f86d5cf64d1d
Waheed Khaled <waheedkhaled95@gmail.com>
2024-01-23 23:51:07 +0200

    Betty Style Check

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -5,56 +5,56 @@
 
 /**
  * swap - function used to swap two values inside an array
- * first: First integer value
- * second: Second integer value
+ * @first: First integer value
+ * @second: Second integer value
  * Returns: Nothing.
  */
 void swap(int *first, int *second)
 {
-    int temp = *first;
-    *first = *second;
-    *second = temp;
+	int temp = *first;
+	*first = *second;
+	*second = temp;
 }
 
 
 /**
  * selection_sort - function that applies selection sort algorithm.
- * array: Array to apply the algorithm on
- * size: size of the inserted array.
+ * @array: Array to apply the algorithm on
+ * @size: size of the inserted array.
  * Returns: Nothing.
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t current = 0;
-    size_t index = 0;
-    int smallest = array[0], smallest_index = 0;
+	size_t current = 0;
+	size_t index = 0;
+	int smallest = array[0], smallest_index = 0;
 
-    if (size > 2)
-    {
-        while (current < size)
-        {
-            if (array[index] < smallest)
-            {
-                smallest = array[index];
-                smallest_index = index;
-            }
-            /* Reached the end of the array */
-            if (index == size - 1)
-            {    
-                /* swap */
-                swap(&array[current], &array[smallest_index]);
-                /* Increment current */
-                current++;
-                /* restate index */
-                index = current;
-                /* Restate smallest_index and smallest value */
-                smallest_index = current;
-                smallest = array[current];
-                print_array(array, size);
-            }
-            else
-                index++;
-        }
-    }
-    
+	if (size > 2)
+	{
+		while (current < size)
+		{
+			if (array[index] < smallest)
+			{
+				smallest = array[index];
+				smallest_index = index;
+			}
+			/* Reached the end of the array */
+			if (index == size - 1)
+			{
+				/* swap */
+				swap(&array[current], &array[smallest_index]);
+				/* Increment current */
+				current++;
+				/* restate index */
+				index = current;
+				/* Restate smallest_index and smallest value */
+				smallest_index = current;
+				smallest = array[current];
+				print_array(array, size);
+			}
+			else
+				index++;
+		}
+	}
+
 }
