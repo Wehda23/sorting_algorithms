75e0366d92a06c597c45626243817e0db67e4886
Waheed Khaled <waheedkhaled95@gmail.com>
2024-01-24 00:13:11 +0200

    Test

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -29,37 +29,33 @@ void selection_sort(int *array, size_t size)
 	size_t index = 0;
 	int smallest = array[0], smallest_index = 0;
 	int change = 0;
-    
-	if (size > 2)
-	{
-		while (current < size)
-		{
 
-			if (array[index] < smallest)
-			{
-				smallest = array[index];
-				smallest_index = index;
-				change = 1;
-			}
-			/* Reached the end of the array */
-			if (index == size - 1)
-			{
-				/* swap */
-				swap(&array[current], &array[smallest_index]);
-				/* Increment current */
-				current++;
-				/* restate index */
-				index = current;
-				/* Restate smallest_index and smallest value */
-				smallest_index = current;
-				smallest = array[current];
-				if (change == 1)
-					print_array(array, size);
-				change = 0;
-			}
-			else
-				index++;
-		}
-	}
-
+	
+    while (current < size)
+    {
+        if (array[index] < smallest)
+        {
+            smallest = array[index];
+            smallest_index = index;
+            change = 1;
+        }
+        /* Reached the end of the array */
+        if (index == size - 1)
+        {
+            /* swap */
+            swap(&array[current], &array[smallest_index]);
+            /* Increment current */
+            current++;
+            /* restate index */
+            index = current;
+            /* Restate smallest_index and smallest value */
+            smallest_index = current;
+            smallest = array[current];
+            if (change == 1)
+                print_array(array, size);
+            change = 0;
+        }
+        else
+            index++;
+    }
 }
