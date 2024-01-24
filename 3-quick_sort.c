aba351220a7f1766986b04fdf021717d0368d76c
Waheed Khaled <waheedkhaled95@gmail.com>
2024-01-24 02:20:03 +0200

    Fix

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -25,22 +25,23 @@ void swap(int *first, int *second)
  * @size: size of the array
  * Returns: Nothing.
  */
-size_t partition(int *arr, size_t low, size_t high, size_t size) {
-    int pivot = arr[high];
-    size_t i = low - 1, j;
+size_t partition(int *arr, size_t low, size_t high, size_t size)
+{
+	int pivot = arr[high];
+	size_t i = low - 1, j;
 
-    for (j = low; j <= high - 1; j++)
-    {
-        if (arr[j] < pivot)
-        {
-            i++;
-            swap(&arr[i], &arr[j]);
-        }
-    }
+	for (j = low; j <= high - 1; j++)
+	{
+		if (arr[j] < pivot)
+		{
+			i++;
+			swap(&arr[i], &arr[j]);
+		}
+	}
 
-    swap(&arr[i + 1], &arr[high]);
-    print_array(arr, size);
-    return i + 1;
+	swap(&arr[i + 1], &arr[high]);
+	print_array(arr, size);
+	return (i + 1);
 }
 
 /**
@@ -53,33 +54,33 @@ size_t partition(int *arr, size_t low, size_t high, size_t size) {
  */
 void quick_sort_helper(int *arr, size_t low, size_t high, size_t size)
 {
-    size_t pivot_index;
+	size_t pivot_index;
 
-    if (low < high)
-    {
-        pivot_index = partition(arr, low, high, size);
+	if (low < high)
+	{
+		pivot_index = partition(arr, low, high, size);
 
-        if (pivot_index > 0)
-        {
-            quick_sort_helper(arr, low, pivot_index - 1, size);
-        }
+		if (pivot_index > 0)
+		{
+			quick_sort_helper(arr, low, pivot_index - 1, size);
+		}
 
-        quick_sort_helper(arr, pivot_index + 1, high, size);
-    }
+		quick_sort_helper(arr, pivot_index + 1, high, size);
+	}
 }
 
 /**
  * quick_sort - function that implements Quick Sort Algorithm
- * @array: Array to apply the algorithm to 
+ * @array: Array to apply the algorithm to
  * @size: Size of the array
  * Returns: Nothing.
  */
 void quick_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-    {
-        return;
-    }
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
 
-    quick_sort_helper(array, 0, size - 1, size);
+	quick_sort_helper(array, 0, size - 1, size);
 }
